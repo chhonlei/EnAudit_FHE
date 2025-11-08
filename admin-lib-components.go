f7e77e2b952567aac7daacea2c75dd1063c8ae58
chhonlei <chhonlei@users.noreply.github.com>
2025-11-08 09:01:22 +0000

    Add admin-lib-components.go, ape.js, ZamaConfig.dbg.json

---
+++ admin-lib-components.go
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/enaudit_fhe/admin-lib-components.go
+// Generated at: 2025-12-03T23:45:45.721Z
+
---
+++ ape.js
@@ -0,0 +1,4 @@
+import { ethers } from 'ethers';
+
+interface lib_distProps { data: string }
+
